33ceb1c24a13b9cd17c195f2cb4f96b5b67a7b26
Álvaro Pérez <alvarojperezperez@gmail.com>
2020-04-23 11:12:40 +0200

    Khansole academy problem completed

--- khansole_academy.py
+++ khansole_academy.py
@@ -6,15 +6,37 @@ Add your comments here.
 
 import random
 
+PROBLEMS_CORRECT_NEEDED = 3
+MIN_RANDOM = 10
+MAX_RANDOM = 99
 
 def main():
-    """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
-    """
-    pass
+    # Define the variable that will count the answers correct in a row
+    correct_in_row = 0
+
+    # Loop the whole program until the user has as many correct answers in a row as PROBLEMS_CORRECT_NEEDED
+    while correct_in_row < PROBLEMS_CORRECT_NEEDED:
+        # Generate two random numbers between MIN_RANDOM and MAX_RANDOM
+        num1 = random.randint(MIN_RANDOM, MAX_RANDOM)
+        num2 = random.randint(MIN_RANDOM, MAX_RANDOM)
+        # Calculate the correct answer
+        total = num1 + num2
+
+        # Print the operation and get the user input
+        print ("What is " + str(num1) + " + " + str(num2) + "?")
+        answer = int(input("Your answer: "))
+
+        # Check if the user answer is the same as the correct answer
+        if answer == total:
+            # If the answer is correct, add 1 to the count of problems correct in a row
+            correct_in_row += 1
+            # Print to the user that their answer is correct and how many problems correct in a row they have.
+            print("Correct!   "  + "You've gotten " + str(correct_in_row) + " correct in a row." + "\n")
+        else:
+            # If the answer was not correct, reset the correct in a row count
+            correct_in_row = 0
+            # And let the user know their answer was incorrect. Print what the correct answer is.
+            print ("Incorrect.   " + "The expected answer is " + str(total) + "\n")
 
 
 # This provided line is required at the end of a Python file
