29b71394e1421a8871f6dbb5f699e39a02fb9108
Álvaro Pérez <alvarojperezperez@gmail.com>
2020-04-23 10:46:25 +0200

    Liftoff problem completed

--- liftoff.py
+++ liftoff.py
@@ -7,13 +7,16 @@ It counts down the numbers from 10 to 1 and then writes “Liftoff!”
 
 
 def main():
-    """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
-    """
-    pass
+    # Declare the countdown variable
+    countdown = 10
+    # Loop as many times as numbers are in the countdown
+    for i in range(countdown):
+        # Print the current countdown number at the start of the loop
+        print(countdown)
+        # Subtract 1 from the countdown
+        countdown -= 1
+    # Prints end message to the screen when the loop has finished
+    print("Liftoff!")
 
 
 # This provided line is required at the end of a Python file
